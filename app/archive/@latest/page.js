fc3eea5c91cc9532ec848bacb1efabfc91036de5
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-09 21:05:45 +0000

    default-news-apge

--- app/archive/@latest/page.js
+++ app/archive/@latest/page.js
@@ -1,3 +1,11 @@
+import NewsList from "@/components/news-list";
+import { getLatestNews } from "@/lib/news";
+
 export default function latestNewsPage() {
-    return (<h1>Latest News Page</h1>);
+    const latestNews = getLatestNews();
+    
+        return (<>
+            <h2>Latest News</h2>
+            <NewsList news={latestNews}/>
+        </>);
 }
