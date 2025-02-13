d0bd19e25f322889d37dfc7317f0eeb070a69c21
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-13 19:52:16 +0000

    intercept-route-modal

---
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -0,0 +1,22 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function InterceptedImagePage({ params }) {
+    const newsItemSlug = params.slug;
+    const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemSlug);
+
+    if (!newsItem) {
+        notFound();
+    }
+
+    return (
+        <>
+            <div className="modal-backdrop">
+                <dialog className="modal" open>
+                    <div className="fullscreen-image">
+                        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                    </div>
+                </dialog>
+            </div>
+        </>
+    );
+}
